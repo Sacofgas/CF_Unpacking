adff09bbb2fa24911ae8485653cbe3f0ca3146ea
Marcello Zanghieri <marcello.zanghieri@synsquare.com>
2026-07-22 14:39:52 +0200

    Make minor changes to DLP

--- unpack_dlp_buffer.py
+++ unpack_dlp_buffer.py
@@ -1,3 +1,4 @@
+import json
 from argparse import ArgumentParser, Namespace
 from collections.abc import Iterator
 from ctypes import (
@@ -8,7 +9,6 @@ from ctypes import (
     Structure,
 )
 from datetime import datetime
-from json import dumps as json_dumps
 from typing import Any, Self
 
 
@@ -16,14 +16,16 @@ from typing import Any, Self
 #    FUNCTIONS    #
 ###################
 
-class IterUnpackableBigEndianStructure(BigEndianStructure):
 
+class IterUnpackableBigEndianStructure(BigEndianStructure):
     @classmethod
     def iter_unpack(cls, data: bytes) -> Iterator[Self]:
         # this method is intended to be like `struct.Struct.iter_unpack()`
         size = sizeof(cls)
         if len(data) % size != 0:
-            raise ValueError(f'Iterative unpacking requires a buffer of a multiple of {size} bytes.')
+            raise ValueError(
+                f"Iterative unpacking requires a buffer of a multiple of {size} bytes."
+            )
         num_elems = len(data) // size
         for i_elem in range(num_elems):
             i_byte_start = size * i_elem
@@ -36,12 +38,12 @@ class DLPBufferEntry(IterUnpackableBigEndianStructure):
     # UNI/TS 11291-13-2 (UNI1610630) 5.4.11.4.2
     _pack_ = 1
     _fields_ = [
-        ('unix_time', c_uint32),
-        ('daily_diagnostic', c_uint16),
-        ('curr_index_of_converted_vol', c_uint32),
-        ('curr_index_of_converted_vol_under_alarm', c_uint32),
-        ('max_conventional_converted_gas_flow_value', c_uint32),
-        ('max_conventional_converted_gas_flow_status', c_uint16),
+        ("unix_time", c_uint32),
+        ("daily_diagnostic", c_uint16),
+        ("curr_index_of_converted_vol", c_uint32),
+        ("curr_index_of_converted_vol_under_alarm", c_uint32),
+        ("max_conventional_converted_gas_flow_value", c_uint32),
+        ("max_conventional_converted_gas_flow_status", c_uint16),
     ]
 
 
@@ -50,18 +52,29 @@ def ctypes_struct_2_dict(struct: Structure) -> dict[str, Any]:
 
 
 def unpack_dlp_buffer(dlp_buffer: bytes) -> None:
-    _DATETIME_FMT = '%Y-%m-%d %H:%M:%S'
-    _dd_num_bits = DLPBufferEntry.daily_diagnostic.size * 8
+    _DATETIME_FMT = "%Y-%m-%d %H:%M:%S"
+    _DD_NUM_BITS = (
+        DLPBufferEntry.daily_diagnostic.size * 8
+    )  # daily diagnostic number of bits
+    # get elements
     elems = DLPBufferEntry.iter_unpack(dlp_buffer)
-    expanded_elems = [
-        {
-            'index': i,
-            'datetime': datetime.fromtimestamp(elem.unix_time).strftime(_DATETIME_FMT),
-            'bitwise_daily_diagnostic': f'{elem.daily_diagnostic:0{_dd_num_bits}b}',
-            'data': ctypes_struct_2_dict(elem),
-        } for i, elem in enumerate(elems)
-    ]
-    json_str = json_dumps(expanded_elems, indent=4)
+    # make expanded elements
+    expanded_elems = []
+    for i, elem in enumerate(elems):
+        # get expanded data
+        datetime_ = datetime.fromtimestamp(elem.unix_time).strftime(_DATETIME_FMT)
+        bitwise_dd_str = (f"{elem.daily_diagnostic:0{_DD_NUM_BITS}b}",)
+        data_dict = ctypes_struct_2_dict(elem)
+        # assemble and append expanded element
+        expanded_elem = {
+            "index": i,
+            "datetime": datetime_,
+            "bitwise_daily_diagnostic": bitwise_dd_str,
+            "data": data_dict,
+        }
+        expanded_elems.append(expanded_elem)
+    # serialize and print expanded elements
+    json_str = json.dumps(expanded_elems, indent=4)
     print(json_str)
 
 
@@ -69,16 +82,16 @@ def unpack_dlp_buffer(dlp_buffer: bytes) -> None:
 #    SCRIPTING    #
 ###################
 
+
 def create_argparser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument('data', type=bytes.fromhex, help='input hex data string')
+    parser.add_argument("data", type=bytes.fromhex, help="input hex data string")
     return parser
 
 
 def parse_args() -> Namespace:
     parser = create_argparser()
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 
 def main() -> None:
@@ -86,5 +99,5 @@ def main() -> None:
     unpack_dlp_buffer(args.data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
